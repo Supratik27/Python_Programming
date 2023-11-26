e0ff38d2ae15c2a542f28ae071007de9ea12ae67
Supratik27 <Supratiksengupta272gmail.com>
2023-11-26 13:26:38 +0530

    3rd day

--- Factorial of a Number.py
+++ Factorial of a Number.py
@@ -1,6 +1,6 @@
 fact = 1
 num = int(input("Enter the no :"))
-print("The no you entered is:", num)
+print("The no you entered is:%d" %num)
 if num < 0:
     print("The factorial of the no does not exists")
 elif num == 0:
@@ -9,3 +9,5 @@ else:
     for i in range(1, num+1):
         fact = fact*i
     print("factorial of the no is",fact)
+
+
